25d881fb6cf23a4b9d8f44c0737d4e13db53e699
Mathias <mathiasbdev@gmail.com>
2024-04-03 13:55:06 -0300

    Update/Fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from 'next/app'
 import '../globals.css'
+// Fonts
+/* import localFont from '@next/font/local'
+const futuraLightRegular = localFont({ src: '/fonts/futura-light-regular/font.otf' });
+const optima = localFont({ src: '/fonts/optima/medium.otf' }); */
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <Component {...pageProps} />
+        <div className={``}>
+            <Component {...pageProps} />
+        </div>
     )
 }
