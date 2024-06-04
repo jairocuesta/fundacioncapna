ef1e12d17f835cdf60582edeb52ee8645be327c1
Mathias <mathiasbdev@gmail.com>
2024-06-04 14:36:50 -0300

    Update/Membership images

--- src/components/Banners/BecomeMember.tsx
+++ src/components/Banners/BecomeMember.tsx
@@ -11,7 +11,7 @@ export default function BecomeMember() {
 
     return (
         <div className={`max-w-7xl mx-auto px-5 sm:px-10 lg:px-20 w-full`}>
-            <div className={`${styles.becomeMember} grid place-content-center w-full h-[20rem] sm:h-[30rem] rounded-2xl`} style={{ backgroundImage: `url('/programas/example/${image}.jpg')`}}>
+            <div className={`${styles.becomeMember} grid place-content-center w-full h-[20rem] sm:h-[30rem] rounded-2xl`} style={{ backgroundImage: `url('/programas/banners/${image}.webp')`}}>
                 <Button
                     href={"#"}
                     text={"Conviértete en miembro"}
--- src/components/Banners/HowCanHelp.tsx
+++ src/components/Banners/HowCanHelp.tsx
@@ -10,7 +10,7 @@ export default function HowCanHelp() {
     const image = randomIntFromInterval(3, 4);
 
     return (
-        <div className={`flex items-end ${styles.howCanHelp} h-[35rem] text-white px-5`} style={{ backgroundImage: `url('/programas/example/${image}.jpg')` }}>
+        <div className={`flex items-end ${styles.howCanHelp} h-[35rem] text-white px-5`} style={{ backgroundImage: `url('/programas/banners/${image}.webp')` }}>
             <div className={"flex flex-col justify-center items-center gap-10 h-full w-full text-center"}>
                 <h2 className={"text-xl sm:text-[1.7rem] sm:leading-9"}>
                     <div>Te invitamos a <span className={"font-bold"}>ser parte de<br /> nuestra misión</span> de conservación</div>
