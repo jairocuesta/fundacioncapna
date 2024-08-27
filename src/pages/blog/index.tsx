abffca388a2f3d4dd50d92477fbfe9b69fd73ef1
Mathias <mathiasbdev@gmail.com>
2024-08-27 16:56:11 -0300

    blog page title

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -74,7 +74,7 @@ export default function Blog({ data, page, pageCount }: { data: BlogType[]; page
                         className={"relative flex flex-col gap-4 px-5 sm:px-10 lg:px-28 py-20 w-full max-w-6xl mx-auto text-center"}
                     >
                         <h1 className={"flex items-center gap-3 text-2xl md:text-3xl lg:text-4xl lg:leading-[3rem] text-shadow"}>
-                            Título de algún blog o artículo que se desee destacar
+                            Blogs
                         </h1>
                        {/*  <div className={"flex items-center justify-center gap-2"}>
                             <div className={"h-1 w-12 bg-white opacity-50 rounded-full"}></div>
